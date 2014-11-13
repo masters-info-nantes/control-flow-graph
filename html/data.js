365e866e7b5c5625d899772fb0e2c5e001c0007e
jbardon <bardonjeremy1@gmail.com>
2014-11-13 21:00:58 +0100

    Add graph visualization with vis.js - may be delete before release

---
+++ html/data.js
@@ -0,0 +1,11 @@
+var nodes = [];
+var edges = [];
+nodes.push({ id: 1560766941, label: String("A")});
+edges.push({ from: 1560766941, to: 964526379});
+nodes.push({ id: 964526379, label: String("0")});
+edges.push({ from: 964526379, to: 1588284297});
+edges.push({ from: 964526379, to: 1072447469});
+edges.push({ from: 964526379, to: 794362212});
+nodes.push({ id: 1588284297, label: String("B")});
+nodes.push({ id: 1072447469, label: String("C")});
+nodes.push({ id: 794362212, label: String("D")});
--- src/main/java/fr/univnantes/controlflowgraph/App.java
+++ src/main/java/fr/univnantes/controlflowgraph/App.java
@@ -1,5 +1,9 @@
 package fr.univnantes.controlflowgraph;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
@@ -8,7 +12,12 @@ import java.util.LinkedList;
  *
  */
 public class App {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		File file = new File("html/data.js");
+		App.display(file);
+	}
+	
+	public static void anthMain(){
 		System.out.println("Hello World!");
 		Node origin,nA,nB,nC,nD,n;
 		Arc a;
@@ -62,5 +71,61 @@ public class App {
 			n = new Instruction("inst "+i);
 			System.out.println(n.toString());
 		}
+	}
+	
+	/**
+	 * Generate js code to display graph in web browser
+	 * 	- html folder in root directory contains vis.js library
+	 *  - to see graph see html/index.html in a browser
+	 *  - not sure to keep for future release
+	 * @param file
+	 * @throws IOException
+	 */
+	public static void display(File file) throws IOException {
+		Node origin,nA,nB,nC,nD,n;
+		Arc a;
+		
+		origin = new Instruction("x = 8");
+		
+		nA = new Condition();
+		a = new Arc(" ",nA);
+		origin.addArc(a);
+		
+		nB = new Instruction("y = 6");
+		a = new Arc("x > 8",nB);
+		nA.addArc(a);
+		
+		nC = new Instruction("y = 7");
+		a = new Arc("x == 8",nC);
+		nA.addArc(a);
+		
+		nD = new Instruction("y = 1");
+		a = new Arc(" ",nD);
+		nA.addArc(a);
+		
+		//-----
+		String path = file.getAbsolutePath();
+		file.delete();
+		BufferedWriter nfile = new BufferedWriter(new FileWriter(path, true));
+		nfile.write("var nodes = [];\n");
+		nfile.write("var edges = [];\n");
+		//-----
+		
+		// Print example
+		LinkedList<Node> queue = new LinkedList<Node>();
+		queue.add(origin);
+		Node cur = null;
+		while(queue.size() > 0) {
+			cur = queue.remove();
+			nfile.write("nodes.push({ id: " + cur.hashCode() + ", label: String(\"" + cur.getName() + "\")});\n");
+
+			System.out.println(cur.toString());	
+			for(Arc arc : cur.getArcs()) {
+				nfile.write("edges.push({ from: " + cur.hashCode() + ", to: " + arc.getNext().hashCode() + "});\n");
+				queue.add(arc.getNext());
+			}
+		}
+		
+		nfile.close();
 	}
 }
