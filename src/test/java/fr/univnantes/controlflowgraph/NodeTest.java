00368c2d64508e0fe8dd3c7e7fd7c1c9fc966512
jbardon <bardonjeremy1@gmail.com>
2014-11-21 14:34:39 +0100

    Change js generated code to declare all nodes befores arcs

--- src/test/java/fr/univnantes/controlflowgraph/NodeTest.java
+++ src/test/java/fr/univnantes/controlflowgraph/NodeTest.java
@@ -6,6 +6,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 import org.junit.Before;
@@ -92,17 +93,38 @@ public class NodeTest {
 		nfile.write("var nodes = [];\n");
 		nfile.write("var edges = [];\n");
 		
-		// Print example
+		// 2 loops because all nodes must declared before be mentionned in edges
+		boolean start = true;
 		LinkedList<Node> queue = new LinkedList<Node>();
 		queue.add(graph);
 		Node cur = null;
+		ArrayList<Integer> idList = new ArrayList<Integer>();
 		while(queue.size() > 0) {
 			cur = queue.remove();
-			nfile.write("nodes.push({ id: " + cur.hashCode() + ", label: String(\"" + cur.getName() + "\")});\n");
-
-			System.out.println(cur.toString());	
+			if(!idList.contains(cur.hashCode())){
+				if(start){
+					nfile.write("nodes.push({ id: " + cur.hashCode() + ", label: String(\"" + cur.getName() + "\"), title: \"Start node\" });\n");
+					start = false;
+				}
+				else {
+					nfile.write("nodes.push({ id: " + cur.hashCode() + ", label: String(\"" + cur.getName() + "\"), title: String(\"" + cur.getLabel() + "\") });\n");		
+				}	
+			}
+			
+			idList.add(cur.hashCode());
+			
 			for(Arc arc : cur.getArcs()) {
-				nfile.write("edges.push({ from: " + cur.hashCode() + ", to: " + arc.getNext().hashCode() + "});\n");
+				queue.add(arc.getNext());
+			}
+		}
+		
+		queue.add(graph);
+		cur = null;
+		while(queue.size() > 0) {
+			cur = queue.remove();			
+
+			for(Arc arc : cur.getArcs()) {
+				nfile.write("edges.push({ from: " + cur.hashCode() + ", to: " + arc.getNext().hashCode() + " });\n");
 				queue.add(arc.getNext());
 			}
 		}
